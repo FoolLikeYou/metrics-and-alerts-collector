e477cb2a8931678288eeead5f0047ada971fdfed
Yarovoy <lambda2712@gmail.com>
2026-02-23 13:38:25 +0300

    Инкремент 1

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"metrics-and-alerts-collector/internal/handler"
+	"metrics-and-alerts-collector/internal/repository"
+	"net/http"
+)
+
+func main() {
+	st := repository.NewMemStorage()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", handler.UpdateHandler(st))
+	mux.HandleFunc("/metrics", handler.ListHandler(st))
+
+	_ = http.ListenAndServe(":8080", mux)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module metrics-and-alerts-collector
+
+go 1.24.3
