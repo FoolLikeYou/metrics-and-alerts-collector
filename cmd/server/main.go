b403e1668642f9e2dc79c89a686966a7507f27b2
Yarovoy <lambda2712@gmail.com>
2026-04-14 09:03:08 +0300

    incr 5

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,19 +4,28 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/Yandex-Practicum/go-musthave-metrics-tpl/internal/server"
 	"github.com/Yandex-Practicum/go-musthave-metrics-tpl/internal/storage"
 )
 
 func main() {
-	addr := flag.String("a", "localhost:8080", "HTTP server listen address")
+	addrFlag := flag.String("a", "localhost:8080", "HTTP server listen address")
 	flag.Parse()
+
+	addr := *addrFlag
+	if v, ok := os.LookupEnv("ADDRESS"); ok {
+		if t := strings.TrimSpace(v); t != "" {
+			addr = t
+		}
+	}
 
 	store := storage.NewMemStorage()
 	mux := server.NewRouter(store)
 
-	if err := http.ListenAndServe(*addr, mux); err != nil {
+	if err := http.ListenAndServe(addr, mux); err != nil {
 		log.Fatal(err)
 	}
 }
